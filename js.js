1d506640030082fbeac85a8500c0101c425aeb03
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-05-22 20:03:14 +0300

    Update js.js

--- js.js
+++ js.js
@@ -118,16 +118,16 @@ function getFromDB(User) {
     //});
 	if (User != 'No user signed in!') {
 		var table = document.createElement('table');
-		onValue(ref(database, 'Frvr/Clienti'), function(snapshot) {
+		onValue(ref(database, 'Frvr/Clienti/1'), function(snapshot) {
 			snapshot.forEach(function(ChildSnapshot) {
 				var id = ChildSnapshot.key;
+				console.log(ChildSnapshot.key);
 				var tr = document.createElement('tr');
 				var td = document.createElement('td');
 				var br1 = document.createElement('BR');
 				var br2 = document.createElement('BR');
 				var text1 = document.createTextNode(id + '. ' + ChildSnapshot.val().Nume);
 				var text2 = document.createTextNode('Detalii: ' + ChildSnapshot.val().Detalii);
-				if (id == 1) { console.log(snapshot.key); }
 				var text3 = document.createTextNode('Follow up: ' +  ChildSnapshot.val().Followup);
 				td.appendChild(text1);
 				td.appendChild(br1);
