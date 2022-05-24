02b90ff644d6ebf410f2011fb4bc2b6b69e1da24
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-05-24 16:13:46 +0300

    Update js.js

--- js.js
+++ js.js
@@ -114,6 +114,9 @@ function getFromDB(User) {
 				var tr1 = document.createElement('tr');
 				var tr2 = document.createElement('tr');
 				var tr3 = document.createElement('tr');
+					tr1.classList.add("row_" + i);
+					tr2.classList.add("row_" + i);
+					tr3.classList.add("row_" + i);
 				var tr1td1 = document.createElement('td');
 				tr1td1.id = "col1_id";
 				var tr1td2 = document.createElement('td');
@@ -212,45 +215,6 @@ function editdone(td, isOk) {
   }
   td.classList.remove('edit-td');
   editTB = null;
-}
-
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("table");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-    for (i = 0; i < tr.length; i++) {
-    	td = tr[i].getElementsByTagName("td")[1];
-		if (tr[i].style.display == "") {
-    		if (i % 3 == 0) {
-            	tr[i+1].style.display = "";
-                tr[i+2].style.display = "";
-                i = i + 2;
-            }
-            if (i % 3 == 1) {
-				tr[i-1].style.display = "";
-                tr[i+1].style.display = "";
-                i = i + 2;
-            }
-            if (i % 3 == 2) {
-				tr[i-2].style.display = "";
-                tr[i-1].style.display = "";
-                i = i + 2;
-            }
-		}
-    }       
 }
 
 function saveToDB() {
